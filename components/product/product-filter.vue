6ae2b66b7600fc7dcc864639fba5c0ffbfec3116
Eric Hidalgo <ehidalgo@iadb.org>
2020-06-08 02:44:54 -0300

    fix: styles css fixed

--- components/product/product-filter.vue
+++ components/product/product-filter.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="flex flex-wrap md:flex-no-wrap items-center justify-between mx-auto pt-5 pb-5 mt-16">
+  <div class="flex flex-wrap md:flex-no-wrap items-center justify-between mx-auto mt-16">
     <div class="flex items-center">
-      <span class="text-2xl font-normal tracking-tight mr-3">16 of 32 products | Sort by:</span> 
+      <span class="text-primary-text text-2xl font-normal tracking-tight mr-3">16 of 32 products</span>
+      <div class="border-l mx-6 h-12"></div>
+      <span class="text-secondary-text text-2xl font-normal tracking-tight mr-3">Sort by:</span>
       <product-filter-button label="Most recent"></product-filter-button>
       <product-filter-button label="Lowest price"></product-filter-button>
       <product-filter-button label="Highest price"></product-filter-button>
     </div>
     <div class="flex items-center">
-      <img src="arrow-left.svg" class="mr-3"/>
-      <img src="arrow-right.svg"/>
+      <button class="mr-3">
+        <img src="arrow-left.svg"/>
+      </button>
+      <button>
+        <img src="arrow-right.svg"/>
+      </button>
     </div>
   </div>
 </template>
