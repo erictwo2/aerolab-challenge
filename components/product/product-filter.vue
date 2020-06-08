44d382266f9800a8311ab47e11712b317eb42e01
Eric Hidalgo <ehidalgo@iadb.org>
2020-06-08 02:07:37 -0300

    fix: minor changes

--- components/product/product-filter.vue
+++ components/product/product-filter.vue
@@ -1,18 +1,16 @@
 <template>
-  <header class="bg-white">
-    <div class="flex flex-wrap md:flex-no-wrap items-center justify-between mx-auto pt-5 pb-5 mt-16 ml-32 mr-32">
-      <div class="flex items-center">
-        <span class="text-2xl font-normal tracking-tight mr-3">16 of 32 products | Sort by:</span> 
-        <product-filter-button label="Most recent"></product-filter-button>
-        <product-filter-button label="Lowest price"></product-filter-button>
-        <product-filter-button label="Highest price"></product-filter-button>
-      </div>
-      <div class="flex items-center">
-        <img src="arrow-left.svg" class="mr-3"></img>
-        <img src="arrow-right.svg"></img>
-      </div>
+  <div class="flex flex-wrap md:flex-no-wrap items-center justify-between mx-auto pt-5 pb-5 mt-16">
+    <div class="flex items-center">
+      <span class="text-2xl font-normal tracking-tight mr-3">16 of 32 products | Sort by:</span> 
+      <product-filter-button label="Most recent"></product-filter-button>
+      <product-filter-button label="Lowest price"></product-filter-button>
+      <product-filter-button label="Highest price"></product-filter-button>
     </div>
-  </header>
+    <div class="flex items-center">
+      <img src="arrow-left.svg" class="mr-3"/>
+      <img src="arrow-right.svg"/>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
