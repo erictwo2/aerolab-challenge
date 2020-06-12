ecd3cc23b41de7b9fee6b9630c319891e1bd5e8a
Eric Hidalgo <ehidalgo@iadb.org>
2020-06-11 22:07:29 -0300

    refactor: product sort button removed

--- components/products/product-grid-sorting.vue
+++ components/products/product-grid-sorting.vue
@@ -2,20 +2,15 @@
   <div class="flex flex-no-wrap items-center">
     <div class="border-l w-1 mx-6 h-12"></div>
     <span class="text-secondary-font text-2xl font-normal tracking-tight mr-3">Sort by:</span>
-    <product-sort-button label="Lowest price"></product-sort-button>
-    <product-sort-button label="Highest price"></product-sort-button>
+    <button class="rounded-full h-12 px-6 pt-1 pb-3 mx-3 text-2xl text-white tracking-tight bg-active">Most recent</button>
+    <button class="rounded-full h-12 px-6 pt-1 pb-3 mx-3 text-2xl text-secondary-font tracking-tight bg-gray-200">Lowest price</button>
+    <button class="rounded-full h-12 px-6 pt-1 pb-3 mx-3 text-2xl text-secondary-font tracking-tight bg-gray-200">Highest price</button>
   </div>
 </template>
 
 <script lang="ts">
-
-import ProductSortButton from '~/components/products/product-sort-button.vue'
 import { Vue, Component } from 'vue-property-decorator'
 
-@Component({
-  components: {
-    'product-sort-button': ProductSortButton
-  }
-})
+@Component
 export default class ProductGridSorting extends Vue { }
 </script>
