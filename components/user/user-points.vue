9a86246a36532b6348918bfd5aa129c8487344ee
Eric Hidalgo <ehidalgo@iadb.org>
2020-06-13 15:49:00 -0300

    feat: added skeleton loader

--- components/user/user-points.vue
+++ components/user/user-points.vue
@@ -1,9 +1,18 @@
 <template>
-  <div class="flex items-center">
-    <span v-if="this.user" class="text-primary-font text-2xl">{{ this.user.name }}</span>
-    <div class="rounded-full ml-4 pr-2 pl-3 pt-2 pb-2 bg-gray-200 flex">
-      <span v-if="this.user" class="text-primary-font text-2xl pr-1">{{ this.user.points }}</span>
-      <img class="pt-1" src="coin.svg"/>
+  <div>
+    <div v-if="user" class="flex items-center">
+      <span class="text-primary-font text-2xl">{{ user.name }}</span>
+      <div class="rounded-full h-12 ml-4 pr-2 pl-4 py-1 items-center bg-gray-200 flex">
+        <span class="text-primary-font text-2xl pr-1">{{ user.points }}</span>
+        <img class="pt-1" src="coin.svg"/>
+      </div>
+    </div>
+    <div v-if="!user" class="flex items-center">
+      <skeleton-loader-vue type="text" :width="100" :height="24" animation="fade"/>
+      <div class="rounded-full h-12 ml-4 pr-4 pl-4 py-1 items-center bg-gray-200 flex">
+        <skeleton-loader-vue class="pr-2" type="text" :width="50" :height="24" animation="fade"/>
+        <skeleton-loader-vue class="" type="circle" :width="24" :height="24" animation="fade"/>
+      </div>
     </div>
   </div>
 </template>
@@ -13,13 +22,19 @@ import { Vue, Component } from 'vue-property-decorator'
 import { getModule } from 'vuex-module-decorators'
 import UserModule from '@/store/modules/user-module'
 import { User } from '../../models/user';
+// @ts-ignore
+import SkeletonLoaderVue from 'skeleton-loader-vue'
 
-@Component
+@Component({
+  components: {
+    'skeleton-loader-vue': SkeletonLoaderVue
+  }
+})
 export default class UserPoints extends Vue {
 
   userModule = getModule(UserModule);
 
-  async mounted() {
+  async created() {
     await this.userModule.getUser();
   }
 
