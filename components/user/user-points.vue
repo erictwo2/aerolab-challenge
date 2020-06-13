f1f4b48def3194cff2a32936c11d12843ba553a1
Eric Hidalgo <ehidalgo@iadb.org>
2020-06-13 16:47:47 -0300

    refactor: user changed by prop

--- components/user/user-points.vue
+++ components/user/user-points.vue
@@ -7,40 +7,19 @@
         <img class="pt-1" src="coin.svg"/>
       </div>
     </div>
-    <div v-if="!user" class="flex items-center">
-      <skeleton-loader-vue type="text" :width="100" :height="24" animation="fade"/>
-      <div class="rounded-full h-12 ml-4 pr-4 pl-4 py-1 items-center bg-gray-200 flex">
-        <skeleton-loader-vue class="pr-2" type="text" :width="50" :height="24" animation="fade"/>
-        <skeleton-loader-vue class="" type="circle" :width="24" :height="24" animation="fade"/>
-      </div>
-    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
+import { Vue, Component, Prop } from 'vue-property-decorator'
 import { getModule } from 'vuex-module-decorators'
 import UserModule from '@/store/modules/user-module'
 import { User } from '../../models/user';
-// @ts-ignore
-import SkeletonLoaderVue from 'skeleton-loader-vue'
 
-@Component({
-  components: {
-    'skeleton-loader-vue': SkeletonLoaderVue
-  }
-})
+@Component
 export default class UserPoints extends Vue {
 
-  userModule = getModule(UserModule);
-
-  async created() {
-    await this.userModule.getUser();
-  }
-
-  get user(): User | null {
-    return this.userModule.user;
-  }
+  @Prop({ type: Object, required: true }) readonly user!: User
 
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="flex flex-col min-h-screen">
     <the-header>
-      <user-points></user-points>
+      <user-points v-if="user" :user="user"></user-points>
+      <user-points-skeleton v-if="!user"></user-points-skeleton>
     </the-header>
     <the-subheader title="Electronics"></the-subheader>
     <product-grid :sizePerPage="16"></product-grid>
@@ -14,14 +15,31 @@ import TheHeader from '@/layouts/the-header.vue'
 import TheSubheader from '@/layouts/the-subheader.vue'
 import ProductGrid from '@/components/products/product-grid.vue'
 import UserPoints from '@/components/user/user-points.vue'
+import UserPointsSkeleton from '@/components/user/user-points-skeleton.vue'
+import { getModule } from 'vuex-module-decorators'
+import UserModule from '../store/modules/user-module'
+import { User } from '../models/user'
 
 @Component({
   components: {
     'the-header': TheHeader,
     'the-subheader': TheSubheader,
     'product-grid': ProductGrid,
-    'user-points': UserPoints
+    'user-points': UserPoints,
+    'user-points-skeleton': UserPointsSkeleton
   }
 })
-export default class ProductPage extends Vue { }
+export default class ProductPage extends Vue {
+
+  userModule = getModule(UserModule);
+
+  async created() {
+    await this.userModule.getUser();
+  }
+
+  get user(): User | null {
+    return this.userModule.user;
+  }
+
+ }
 </script>
