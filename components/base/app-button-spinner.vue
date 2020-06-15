fcd3a73833517471a4f427ffa53a549e0f5d333b
Eric Hidalgo <ehidalgo@iadb.org>
2020-06-15 04:54:05 -0300

    test: app button spinner test added

--- components/base/app-button-spinner.vue
+++ components/base/app-button-spinner.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="flex flex-no-wrap justify-center items-center mb-1">
-    <button v-if="!isLoading && !success && !error" class="rounded-full h-12 px-16 pt-2 pb-2 text-lg text-primary-font tracking-tight bg-gray-200">
+    <button id="button" v-if="!isLoading && !success && !error" class="rounded-full h-12 px-16 pt-2 pb-2 text-lg text-primary-font tracking-tight bg-gray-200">
       {{ buttonLabel }}
     </button>
-    <div v-if="isLoading" class="rounded-full h-12 w-12 px-3 pt-3 bg-gray-200">
+    <div id="loader" v-if="isLoading" class="rounded-full h-12 w-12 px-3 pt-3 bg-gray-200">
       <div class="loader ease-linear rounded-full border-4 border-t-4 border-gray-400 h-6 w-6"></div>
     </div>
-    <img v-if="success" class="h-12 w-12" src="~/assets/icons/check.svg">
-    <img v-if="error" class="h-12 w-12" src="~/assets/icons/error.svg">
+    <img id="successIcon" v-if="success" class="h-12 w-12" src="~/assets/icons/check.svg">
+    <img id="errorIcon" v-if="error" class="h-12 w-12" src="~/assets/icons/error.svg">
   </div>
 </template>
 
