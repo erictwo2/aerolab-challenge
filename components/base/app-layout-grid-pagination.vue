2b3e709a8a402e03397d1663f2dcaa038b5d7285
Eric Hidalgo <ehidalgo@iadb.org>
2020-06-15 04:54:29 -0300

    test: app layout grid pagination test added

--- components/base/app-layout-grid-pagination.vue
+++ components/base/app-layout-grid-pagination.vue
@@ -2,16 +2,16 @@
   <div class="container mx-auto px-4 mb-8 md:px-12">
     <div class="flex flex-wrap md:flex-no-wrap items-center justify-between mx-auto mt-16">
       <div class="flex flex-no-wrap items-center">
-        <span class="text-primary-font text-2xl font-normal tracking-tight mr-3">
+        <span id="description" class="text-primary-font text-2xl font-normal tracking-tight mr-3">
           {{ page.size * page.currentPage }} of {{ page.total }} {{ entityName }}
         </span>
         <slot name="sorting"></slot>
       </div>
       <div class="flex items-center">
-        <a v-if="page.prevPage" v-bind:href="page.prevPage" class="cursor-pointer">
+        <a id="prevPage" v-if="page.prevPage" v-bind:href="page.prevPage" class="cursor-pointer">
           <img src="arrow-left.svg"/>
         </a>
-        <a v-if="page.nextPage" v-bind:href="page.nextPage" class="ml-3 cursor-pointer">
+        <a id="nextPage" v-if="page.nextPage" v-bind:href="page.nextPage" class="ml-3 cursor-pointer">
           <img src="arrow-right.svg"/>
         </a>
       </div>
---
+++ test/components/base/app-layout-grid-pagination.spec.ts
@@ -0,0 +1,185 @@
+import { Wrapper, shallowMount } from '@vue/test-utils'
+import AppLayoutGridPagination from "@/components/base/app-layout-grid-pagination.vue";
+import { Page } from '~/models/page';
+
+describe('App Layout Grid Pagination', () => {
+
+  test('should be created', () => {
+
+    let page: Page<any> = {
+      prevPage: null,
+      nextPage: null,
+      currentPage: 1,
+      size: 10,
+      total: 100,
+      sortField: null,
+      sortDirection: null,
+      data: []
+    };
+    let actualProps = {
+      page: page,
+      entityName: 'products'
+    }
+
+    let wrapper: Wrapper<AppLayoutGridPagination> = shallowMount(AppLayoutGridPagination, {
+      propsData: actualProps
+    });
+
+    expect(wrapper.exists()).toBeTruthy();
+  })
+
+  test('should show a description for first page', () => {
+
+    let page: Page<any> = {
+      prevPage: null,
+      nextPage: null,
+      currentPage: 1,
+      size: 10,
+      total: 100,
+      sortField: null,
+      sortDirection: null,
+      data: []
+    };
+    let actualProps = {
+      page: page,
+      entityName: 'products'
+    }
+
+    let wrapper: Wrapper<AppLayoutGridPagination> = shallowMount(AppLayoutGridPagination, {
+      propsData: actualProps
+    });
+
+    let expectedDescription = wrapper.find('#description');
+    expect(expectedDescription.exists()).toBeTruthy();
+    expect(expectedDescription.text()).toBe('10 of 100 products');
+  })
+
+  test('should show a description for tenth page', () => {
+
+    let page: Page<any> = {
+      prevPage: null,
+      nextPage: null,
+      currentPage: 10,
+      size: 10,
+      total: 100,
+      sortField: null,
+      sortDirection: null,
+      data: []
+    };
+    let actualProps = {
+      page: page,
+      entityName: 'products'
+    }
+
+    let wrapper: Wrapper<AppLayoutGridPagination> = shallowMount(AppLayoutGridPagination, {
+      propsData: actualProps
+    });
+
+    let expectedDescription = wrapper.find('#description');
+    expect(expectedDescription.exists()).toBeTruthy();
+    expect(expectedDescription.text()).toBe('100 of 100 products');
+  })
+
+  test('if prevPage is null, the button for previous page should not be shown', () => {
+
+    let page: Page<any> = {
+      prevPage: null,
+      nextPage: null,
+      currentPage: 10,
+      size: 10,
+      total: 100,
+      sortField: null,
+      sortDirection: null,
+      data: []
+    };
+    let actualProps = {
+      page: page,
+      entityName: 'products'
+    }
+
+    let wrapper: Wrapper<AppLayoutGridPagination> = shallowMount(AppLayoutGridPagination, {
+      propsData: actualProps
+    });
+
+    let expectedPrevPage = wrapper.find('#prevPage');
+    expect(expectedPrevPage.exists()).toBeFalsy();
+  })
+
+  test('if prevPage is not empty, the button on the previous page should show', () => {
+
+    let page: Page<any> = {
+      prevPage: '?page=9&size=10',
+      nextPage: null,
+      currentPage: 10,
+      size: 10,
+      total: 100,
+      sortField: null,
+      sortDirection: null,
+      data: []
+    };
+    let actualProps = {
+      page: page,
+      entityName: 'products'
+    }
+
+    let wrapper: Wrapper<AppLayoutGridPagination> = shallowMount(AppLayoutGridPagination, {
+      propsData: actualProps
+    });
+
+    let expectedPrevPage = wrapper.find('#prevPage');
+    expect(expectedPrevPage.exists()).toBeTruthy();
+    expect(expectedPrevPage.attributes('href')).toBe('?page=9&size=10');
+  })
+
+  test('if nextPage is null, the button for next page should not be shown', () => {
+
+    let page: Page<any> = {
+      prevPage: null,
+      nextPage: null,
+      currentPage: 5,
+      size: 10,
+      total: 100,
+      sortField: null,
+      sortDirection: null,
+      data: []
+    };
+    let actualProps = {
+      page: page,
+      entityName: 'products'
+    }
+
+    let wrapper: Wrapper<AppLayoutGridPagination> = shallowMount(AppLayoutGridPagination, {
+      propsData: actualProps
+    });
+
+    let expectedNextPage = wrapper.find('#nextPage');
+    expect(expectedNextPage.exists()).toBeFalsy();
+  })
+
+  test('if nextPage is not empty, the button on the next page should show', () => {
+
+    let page: Page<any> = {
+      prevPage: null,
+      nextPage: '?page=6&size=10',
+      currentPage: 5,
+      size: 10,
+      total: 100,
+      sortField: null,
+      sortDirection: null,
+      data: []
+    };
+    let actualProps = {
+      page: page,
+      entityName: 'products'
+    }
+
+    let wrapper: Wrapper<AppLayoutGridPagination> = shallowMount(AppLayoutGridPagination, {
+      propsData: actualProps
+    });
+
+    let expectedNextPage = wrapper.find('#nextPage');
+    expect(expectedNextPage.exists()).toBeTruthy();
+    expect(expectedNextPage.attributes('href')).toBe('?page=6&size=10');
+  })
+
+})
