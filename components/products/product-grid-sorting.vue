37b460cd92003cd746fff327eb5f486e4aae9c98
Eric Hidalgo <ehidalgo@iadb.org>
2020-06-15 21:05:39 -0300

    refactor: refactored to typescript without classes

--- components/products/product-grid-sorting.vue
+++ components/products/product-grid-sorting.vue
@@ -2,52 +2,61 @@
   <div class="flex flex-no-wrap items-center">
     <div class="border-l w-1 mx-6 h-12"></div>
     <span class="text-secondary-font text-2xl font-normal tracking-tight mr-3">Sort by:</span>
-    <n-link :to="{ path: '/', query: { page: this.page.currentPage, size: this.page.size }}" :class="links.mostRecentClass">
+    <n-link id="mostRecent" :to="{ path: '/', query: { page: this.page.currentPage, size: this.page.size }}" :class="links.mostRecentClass">
       Most recent
     </n-link>
-    <n-link :to="{ path: '/', query: { page: this.page.currentPage, size: this.page.size, sortField: 'cost', sortDirection: 'ASC' }}" :class="links.lowestPriceClass">
+    <n-link id="lowestPrice" :to="{ path: '/', query: { page: this.page.currentPage, size: this.page.size, sortField: 'cost', sortDirection: 'ASC' }}" :class="links.lowestPriceClass">
       Lowest price
     </n-link>
-    <n-link :to="{ path: '/', query: { page: this.page.currentPage, size: this.page.size, sortField: 'cost', sortDirection: 'DESC' }}" :class="links.highestPriceClass">
+    <n-link id="highestPrice" :to="{ path: '/', query: { page: this.page.currentPage, size: this.page.size, sortField: 'cost', sortDirection: 'DESC' }}" :class="links.highestPriceClass">
       Highest price
     </n-link>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component, PropSync, Prop } from 'vue-property-decorator'
-import { Page } from '../../models/page';
+import Vue, { PropOptions } from 'vue'
+import { Page } from '@/models/page'
+import { Product } from '@/models/product'
 
-@Component
-export default class ProductGridSorting extends Vue {
+export default Vue.extend({
 
-  @Prop({ type: Object, required: true }) readonly page!: Page<any>;
+  name: 'product-grid-sorting',
 
-  active = 'rounded-full h-12 px-6 pt-1 pb-3 mx-3 text-2xl tracking-tight bg-gray-200 cursor-pointer bg-active text-white';
-  inactive = 'rounded-full h-12 px-6 pt-1 pb-3 mx-3 text-2xl text-secondary-font tracking-tight bg-gray-200 cursor-pointer';
+  props: {
+    page: {
+      type: Object,
+      required: true
+    } as PropOptions<Page<Product>>
+  },
 
-  get links() {
+  computed: {
+    links: function () {
 
-    let links = {
-      mostRecentClass: this.active,
-      lowestPriceClass: this.inactive,
-      highestPriceClass: this.inactive,
-    };
+      let active = 'rounded-full h-12 px-6 pt-1 pb-3 mx-3 text-2xl tracking-tight bg-gray-200 cursor-pointer bg-active text-white';
+      let inactive = 'rounded-full h-12 px-6 pt-1 pb-3 mx-3 text-2xl text-secondary-font tracking-tight bg-gray-200 cursor-pointer';
 
-    if (this.page.sortField == 'cost' && this.page.sortDirection == 'ASC') {
-      links.mostRecentClass = this.inactive;
-      links.lowestPriceClass = this.active;
-      links.highestPriceClass = this.inactive;
+      let links = {
+        mostRecentClass: active,
+        lowestPriceClass: inactive,
+        highestPriceClass: inactive,
+      };
+
+      if (this.page && this.page.sortField == 'cost' && this.page.sortDirection == 'ASC') {
+        links.mostRecentClass = inactive;
+        links.lowestPriceClass = active;
+        links.highestPriceClass = inactive;
+      }
+
+      if (this.page.sortField == 'cost' && this.page.sortDirection == 'DESC') {
+        links.mostRecentClass = inactive;
+        links.lowestPriceClass = inactive;
+        links.highestPriceClass = active;
+      }
+
+      return links;
     }
-
-    if (this.page.sortField == 'cost' && this.page.sortDirection == 'DESC') {
-      links.mostRecentClass = this.inactive;
-      links.lowestPriceClass = this.inactive;
-      links.highestPriceClass = this.active;
-    }
-
-    return links;
   }
 
-}
+})
 </script>
