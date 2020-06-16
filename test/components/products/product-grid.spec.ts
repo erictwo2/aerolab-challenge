e7b48f7d3dcb1a52ee73e3b9e6b4ee18e3ae5d95
Eric Hidalgo <ehidalgo@iadb.org>
2020-06-16 01:34:51 -0300

    test: product grid test added

---
+++ test/components/products/product-grid.spec.ts
@@ -0,0 +1,31 @@
+import { Wrapper, shallowMount } from '@vue/test-utils'
+import ProductGrid from "@/components/products/product-grid.vue";
+import { Page } from '~/models/page';
+
+describe('Product Grid', () => {
+
+  test('should be created', () => {
+
+    let page: Page<any> = {
+      prevPage: null,
+      nextPage: null,
+      currentPage: 1,
+      size: 10,
+      total: 100,
+      sortField: null,
+      sortDirection: null,
+      data: []
+    };
+    let actualProps = {
+      page: page,
+      sizePerPage: 10
+    };
+
+    let wrapper: Wrapper<ProductGrid> = shallowMount(ProductGrid, {
+      propsData: actualProps
+    });
+
+    expect(wrapper.exists()).toBeTruthy();
+  })
+
+})
